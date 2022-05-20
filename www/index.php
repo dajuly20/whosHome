b7c3ac6875a0a633ce044aff1cf4d71c896feb7e
Julian Wiche <julian@wiche.eu>
2022-05-20 21:15:40 +0100

    new file:   www/img/git.png 	modified:   www/index.php
    Added GitHub Logo and Link ;-)

--- www/index.php
+++ www/index.php
@@ -1,6 +1,7 @@
 <H1>Wer ist zu Hause ;-) </h1>
-<iframe width="100%" height="100%" src="main.php" title="description" id="content"></iframe>
+<small>This is open Source .. <img src=""><a href="https://github.com/dajuly20/whosHome/"><img src="img/git.png" height="15px" witdh="15px"> check out on GitHub</a></small><br>
 
+<iframe width="100%" height="100%" src="main.php" title="description" id="content"></iframe>
 <Script Language="JavaScript">
 setInterval(function () {
 document.getElementById('content').contentWindow.location.reload();
