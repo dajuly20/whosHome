6cbd65136702657a63f9be243074cd9328a775ab
Julian Wiche <julian@wiche.eu>
2022-05-20 20:23:19 +0200

    renamed:    firstSketch.jpeg -> img/firstSketch.jpeg 	new file:   img/keyWall.jpg 	new file:   img/weasleyUhr.jpg 	deleted:    voltageMeasurements.ods 	new file:   www/main.php 	new file:   www/voltageToResistors.csv

---
+++ www/main.php
@@ -0,0 +1,69 @@
+#!/usr/bin/php
+<?php
+ini_set("allow_url_fopen", 1);
+$DBG = false;
+function csvToArray($csvFile){
+ 
+    $file_to_read = fopen($csvFile, 'r');
+ 
+    while (!feof($file_to_read) ) {
+        $lines[] = fgetcsv($file_to_read, 1000, ',');
+ 
+    }
+ 
+    fclose($file_to_read);
+    return $lines;
+}
+
+$personen[0] = "niemand";
+$personen[1] = "Johannes";
+$personen[2] = "Franzi";
+$personen[3] = "Julian";
+$personen[4] = "Reserve";
+$case = "";
+$json = file_get_contents('http://192.168.188.88/status');
+$obj = json_decode($json, true);
+$offset = 0.3;
+$voltage = $obj["adcs"][0]["voltage"];
+
+// Minimal voltage if someone is home shuld be a few volts
+if (round($voltage) != 0){
+   $voltage = $voltage - $offset;
+}
+
+$lookup =  csvToArray("voltageToResistors.csv");
+array_shift($lookup);
+
+$prevDistance = 100;
+echo "Spannung: ". $voltage." V\n";
+
+foreach($lookup as $key => $value){
+ 	if( $value && is_numeric($value[0])){
+        	if($key){
+        		$prevDistance = abs($lookup[$key-1][0] - $voltage);
+        	}
+		$distance =  abs($value[0] - $voltage);
+	
+		if($DBG) echo "\nCase: ".$value[1]. " \t(".$value[0]."  Distanz: ".$distance.") vorh. ".$prevDistance;
+ 	      	if($distance < $prevDistance){
+			$case = $lookup[$key][1];
+			$lookupVoltage = $lookup[$key][0];
+			if($DBG) echo " <== chose that one";
+		}
+	}
+}
+echo "\n\n";
+if(!isset($lookupVoltage)) $lookupVoltage = 0;
+if(!isset($case)) $case = 0;
+
+echo "Wir haben Fall ".$case. " (".$lookupVoltage." V)\n\n";
+$personenDaheim = "";
+for ($i=0; $i < strlen($case); $i++) {
+	$caseNr = $case[$i];
+	$personenDaheim .= $personen[$caseNr]. " ";
+}
+
+echo "Es sing gerade daheim: $personenDaheim\n";
+?>
+
+
